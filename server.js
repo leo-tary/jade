d4b326525924aaba4ab8db7e8b9898868c552ff7
Tarun Gurmukhani <leo.tarun@gmail.com>
2024-03-17 23:01:41 +0530

    updated the score to 99 for player Id 2

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const player = players.find((player) => {
 });
 
 /**
- *  Update player's current score having id 2 to 92
+ *  Update player's current score having id 2 to 99
  */
 
-player["score"] = 92;
+player["score"] = 99;
 console.log(`Updated player`, player);
 console.log(`Players updated`, players);
 console.log(`Unique Reference:`, generateReferenceId());
